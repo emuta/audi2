ada11cf2ca47155f90a2a4cd0855d4a09cd72659
emuta <emuta@mail.com>
2019-04-21 14:04:52 +0800

    format auth-webapp

--- auth/auth-webapp.go
+++ auth/auth-webapp.go
@@ -1,25 +1,25 @@
 package main
 
 import (
-	"os"
 	"encoding/json"
 	"flag"
 	"fmt"
 	"net"
 	"net/http"
+	"os"
 	"strings"
 
-	"google.golang.org/grpc"
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
 
-	_ "audi/pkg/logger"
 	pb "audi/account/proto"
+	_ "audi/pkg/logger"
 )
 
 var (
 	accountServiceClient pb.AccountServiceClient
-	port string
+	port                 string
 )
 
 func init() {
@@ -40,7 +40,6 @@ func init() {
 	log.WithField("account.service.addr", addr).Info("account grpc service address")
 }
 
-
 func main() {
 	addr := fmt.Sprintf("0.0.0.0:%s", port)
 	log.WithField("addr", addr).Info("Server starting")
@@ -49,15 +48,13 @@ func main() {
 
 func NewRouter() *mux.Router {
 	h := NewHandler()
-    r := mux.NewRouter()
-    r.HandleFunc("/login",  h.Login).Methods("POST")
-    r.HandleFunc("/logout", h.Logout).Methods("POST")
-    return r
+	r := mux.NewRouter()
+	r.HandleFunc("/login", h.Login).Methods("POST")
+	r.HandleFunc("/logout", h.Logout).Methods("POST")
+	return r
 }
 
-
 type Handler struct {
-
 }
 
 func NewHandler() *Handler {
@@ -65,67 +62,66 @@ func NewHandler() *Handler {
 }
 
 func (h *Handler) Params(r *http.Request) map[string]string {
-    params := make(map[string]string)
-    values := r.URL.Query()
-    for k, _ := range values {
-        if v := values.Get(k); v != "" {
-            params[k] = strings.TrimSpace(v)
-        }
-    }
-    return params
+	params := make(map[string]string)
+	values := r.URL.Query()
+	for k, _ := range values {
+		if v := values.Get(k); v != "" {
+			params[k] = strings.TrimSpace(v)
+		}
+	}
+	return params
 }
 
-
 func (h *Handler) Error(w http.ResponseWriter, body error, statusCode int) {
-    w.WriteHeader(statusCode)
+	w.WriteHeader(statusCode)
 
-    if err := json.NewEncoder(w).Encode(body); err != nil {
-        fmt.Fprintln(w, err)
-    }
+	if err := json.NewEncoder(w).Encode(body); err != nil {
+		fmt.Fprintln(w, err)
+	}
 }
 
 func (h *Handler) WriteJson(w http.ResponseWriter, body interface{}) {
-    w.Header().Set("Content-type", "application/json;charset=UTF-8")
-    if err := json.NewEncoder(w).Encode(body); err != nil {
-        log.WithError(err).Error("Failed to encode response")
-        log.Errorf("%#v \n", err)
-        h.Error(w, err, 500)
-    }
+	w.Header().Set("Content-type", "application/json;charset=UTF-8")
+	if err := json.NewEncoder(w).Encode(body); err != nil {
+		log.WithError(err).Error("Failed to encode response")
+		log.Errorf("%#v \n", err)
+		h.Error(w, err, 500)
+	}
 }
 
 func (h *Handler) Login(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
-   
-    var req pb.LoginReq
 
-    if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
-        h.Error(w, err, 400)
-        log.WithError(err).Error("Failed to decode request body")
-        log.Errorf("%#v \n", err)
-        return
-    }
+	var req pb.LoginReq
 
-    if host, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
-        req.Ip = host
-    }
-    req.Device = r.UserAgent()
-    req.Fp     = r.Header.Get("Finger-Print")
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		h.Error(w, err, 400)
+		log.WithError(err).Error("Failed to decode request body")
+		log.Errorf("%#v \n", err)
+		return
+	}
 
-    resp, err := accountServiceClient.Login(r.Context(), &req)
+	if host, _, err := net.SplitHostPort(r.RemoteAddr); err == nil {
+		req.Ip = host
+	}
+	req.Device = r.UserAgent()
+	req.Fp = r.Header.Get("Finger-Print")
 
-    if err != nil {
-        h.Error(w, err, 401)
-    }
+	resp, err := accountServiceClient.Login(r.Context(), &req)
 
-    h.WriteJson(w, resp)
+	if err != nil {
+		h.Error(w, err, 401)
+	}
+
+	h.WriteJson(w, resp)
 }
 
 func (h *Handler) Logout(w http.ResponseWriter, r *http.Request) {
-    defer r.Body.Close()
-    var req pb.LogoutReq
-    resp, err := accountServiceClient.Logout(r.Context(), &req)
-    if err != nil {
-        h.Error(w, err, 400)
-    }
-    h.WriteJson(w, resp)
+	defer r.Body.Close()
+	var req pb.LogoutReq
+	resp, err := accountServiceClient.Logout(r.Context(), &req)
+	if err != nil {
+		h.Error(w, err, 400)
+	}
+	h.WriteJson(w, resp)
 }
