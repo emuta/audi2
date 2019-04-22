4acf40e7ae320cb58ff2977191468bb6c9494d09
emuta <emuta@mail.com>
2019-04-22 20:27:42 +0800

    add tables DDL

--- lottery/ssc/cqssc/sql/init.sql
+++ lottery/ssc/cqssc/sql/init.sql
@@ -33,7 +33,48 @@ CREATE SCHEMA cqssc
         group_id int,
         units      int[],
         tag        character varying
-        );
+        )
+    CREATE TABLE term (
+        id bigint primary key,
+        start_from timestamp without time zone,
+        end_to timestamp without time zone,
+        codes character varying[],
+        open_at timestamp without time zone,
+        settled_at timestamp without time zone default null,
+        revoked_at timestamp without time zone default null)
+    CREATE TABLE bet (
+        id         bigserial primary key,
+        created_at timestamp without time zone default current_timestamp,
+        user_id    bigint,
+        odds       numeric(6,4),
+        play_id    integer,
+        unit_id    integer default 1,
+        comm       numeric(6,4) default 0,
+        chase_stop boolean default true,
+        codes      character varying[]
+    )
+    CREATE TABLE bet_plan (
+        id      bigserial primary key,
+        bet_id  bigint,
+        term_id bigint,
+        qty     integer default 1,
+        payment numeric(20,4) default 0,
+        rebate  numeric(20,4) default 0,
+        bonus   numeric(20,4) default 0,
+        times   integer default 1
+    )
+    CREATE TABLE bet.plan_stats (
+        id         bigint PRIMARY KEY,
+        settled    boolean default false,
+        settled_at timestamp without time zone,
+        revoked    boolean default false,
+        revoked_at timestamp without time zone,
+        win        integer  default 0,
+        payment    numeric(20,4)  default 0,
+        rebate     numeric(20,4)  default 0,
+        bonus      numeric(20,4)  default 0,
+        bet_id     bigint
+    );
 
 CREATE VIEW play.vm_play AS (
     select 
